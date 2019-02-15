27fc387efe9b03419afbea9e6945dcbd9a419909
Samuel <samuel-motrico@bol.com.br>
2019-02-15 20:40:44 -0200

    Tentar fazer piscar

--- script.js
+++ script.js
@@ -1,40 +1,73 @@
 let amarelo = document.querySelector('.amarelo');
 let azul = document.querySelector('.azul');
-let vermelho = document.querySelector('.amarelo');
+let vermelho = document.querySelector('.vermelho');
 let verde = document.querySelector('.verde');
-let ordemCores = ['amareloJS', 'azulJS', 'vermelhoJS', 'verdeJS'];
+let ordemCores = ['vermelhoJS', 'amareloJS', 'azulJS'];
 
 
-const aleatorio = function(min, max){
-    return Math.floor(Math.random() * max);
+/*
+let aleatorio = function(min,max){
+    return number = Math.floor(Math.random() * max);
+
 }
+
+aleatorio(0,20);
+console.log(number);
+
+
+
+if(5 > number && number >= 0){
+    ordemCores.push("vermelhoJS");
+    console.log('vermelho');
+};
+if(10 > number && number >= 5){
+    console.log("azul");
+    ordemCores.push("azulJS")
+
+};
+if(15 > number && number >= 10){
+    ordemCores.push("amareloJS")
+    console.log('amarelo');
+};
+if(20 > number && number >= 15){
+    ordemCores.push("verdeJS")
+    console.log('verde');
+};*/
+
 
 const piscar = function(){
     for(let value of ordemCores){
-        // console.log(value);
+        console.log(value);
+        
         if(value == "amareloJS"){
-            amarelo.classList.toggle(value);
+            amarelo.classList.t(value);
             setTimeout(function () {
                 amarelo.classList.toggle(value);
-            }, 1000)
+            }, 1000);
+            setTimeout(piscar,2000);
         }
-        else if(value == "azulJS"){
+        if(value == "azulJS"){
             azul.classList.toggle(value);
-            delay(1000);
-            azul.classList.toggle(value);
-            delay(1000);
+            setTimeout(function () {
+                azul.classList.toggle(value);
+            }, 1000);
+            setTimeout(piscar,2000);
         }
-        else if(value == "vermelhoJS"){
+        if(value == "vermelhoJS"){
             vermelho.classList.toggle(value);
-            delay(1000);
-            vermelho.classList.toggle(value);
-            delay(1000);
+            setTimeout(function () {
+                vermelho.classList.toggle(value);
+            }, 1000);
+            setTimeout(piscar,2000);
         }
-        else if(value == "verdeJS"){
-            verde.classList.toggle(value);
-            delay(1000);
-            verde.classList.toggle(value);
-            delay(1000);
+        if(value == "verdeJS"){ verde.classList.toggle(value);
+            setTimeout(function () {
+                verde.classList.toggle(value);
+            }, 1000);
+            setTimeout(piscar,2000);
+            
         }
+        
     }
 }
+
