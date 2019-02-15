13b7ebbd5b66ea8de15f6f760dad64db01af48ed
Davi Borges <daviborges1000@gmail.com>
2019-02-15 12:07:36 -0200

    Primeira etapa concluida

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-let ordemCores = [];
-
+let amarelo = document.querySelector('.amarelo');
+let azul = document.querySelector('.azul');
+let vermelho = document.querySelector('.amarelo');
+let verde = document.querySelector('.verde');
+let ordemCores = ['amareloJS', 'azulJS', 'vermelhoJS', 'verdeJS'];
 
 
 const aleatorio = function(min, max){
-    return Math.floor(Math.random() * )
+    return Math.floor(Math.random() * max);
+}
+
+const piscar = function(){
+    for(let value of ordemCores){
+        // console.log(value);
+        if(value == "amareloJS"){
+            amarelo.classList.toggle(value);
+            delay(1000);
+            amarelo.classList.toggle(value);
+            delay(1000);
+        }
+        else if(value == "azulJS"){
+            azul.classList.toggle(value);
+            delay(1000);
+            azul.classList.toggle(value);
+            delay(1000);
+        }
+        else if(value == "vermelhoJS"){
+            vermelho.classList.toggle(value);
+            delay(1000);
+            vermelho.classList.toggle(value);
+            delay(1000);
+        }
+        else if(value == "verdeJS"){
+            verde.classList.toggle(value);
+            delay(1000);
+            verde.classList.toggle(value);
+            delay(1000);
+        }
+    }
 }
